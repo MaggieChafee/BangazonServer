87c31b1bb61ffcc64f17f46c1eb74c2de75e5d9e
MaggieChafee <maggie.chafee@gmail.com>
2024-02-24 09:38:58 -0600

    Models Created

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace BangazonServer.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public bool IsCompleted { get; set; }
+        public User Customer {  get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool ShippingRequired { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BangazonServer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime DateCreated { get; set; }
+        public User Vendor { get; set; }
+        public List<Category> Category { get; set; }
+    }
+}
